6835bd163add7bdbf0250c1b59852b0d31b73a07
dominicdotzert <dominicdotzert@gmail.com>
2017-02-08 23:13:37 -0500

    Bug fix

--- Random_Ross.py
+++ Random_Ross.py
@@ -5,7 +5,7 @@ from subprocess import Popen
 def play():
     folder_path = "/home/pi/Videos/Bob Ross/"
     files = os.listdir(folder_path)
-    rand_vid = folder_path + files[randint(0, len(files))]
+    rand_vid = folder_path + files[randint(0, len(files)-1)]
     omxp = Popen(['omxplayer', '-b', rand_vid])
 
 if __name__ == "__main__":
